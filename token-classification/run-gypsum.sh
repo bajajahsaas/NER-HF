aed5dd45cb7f2af0c752e13963f801ca9afa4a2e
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-06-12 02:21:11 -0400

    update ner scripts

--- token-classification/run-gypsum.sh
+++ token-classification/run-gypsum.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=ner
 #SBATCH --output=logsner/ner.txt  # output file
 #SBATCH -e logsner/ner.err        # File to which STDERR will be written
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 #SBATCH --partition=2080ti-long # Partition to submit to
 #SBATCH --mem=40GB
 #
@@ -12,15 +12,35 @@
 export MAX_LENGTH=128
 export BERT_MODEL='/mnt/nfs/work1/696ds-s20/abajaj/nlplab/long-term-context/models/bert-large-cased'
 
-export OUTPUT_DIR='/mnt/nfs/work1/696ds-s20/abajaj/instabase/models/germeval-model'
+# export OUTPUT_DIR='/mnt/nfs/work1/696ds-s20/abajaj/instabase/models/germeval-model-multi'
+
+export OUTPUT_DIR='/mnt/nfs/work1/696ds-s20/abajaj/instabase/models/WNUT-17'
 export BATCH_SIZE=32
 export NUM_EPOCHS=3
 export SAVE_STEPS=750
 export SEED=1
 
+# German NER
+'''
 python3 run_ner.py \
 --data_dir . \
 --labels ./labels.txt \
+--model_name_or_path $BERT_MODEL \
+--output_dir $OUTPUT_DIR \
+--max_seq_length  $MAX_LENGTH \
+--num_train_epochs $NUM_EPOCHS \
+--per_gpu_train_batch_size $BATCH_SIZE \
+--save_steps $SAVE_STEPS \
+--seed $SEED \
+--do_train \
+--do_eval \
+--do_predict
+'''
+
+# English NER
+python3 run_ner.py \
+--data_dir ./data_wnut_17 \
+--labels ./data_wnut_17/labels.txt \
 --model_name_or_path $BERT_MODEL \
 --output_dir $OUTPUT_DIR \
 --max_seq_length  $MAX_LENGTH \
--- token-classification/run.sh
+++ token-classification/run.sh
@@ -1,12 +1,30 @@
-curl -L 'https://sites.google.com/site/germeval2014ner/data/NER-de-train.tsv?attredirects=0&d=1' \
-| grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > train.txt.tmp
-curl -L 'https://sites.google.com/site/germeval2014ner/data/NER-de-dev.tsv?attredirects=0&d=1' \
-| grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > dev.txt.tmp
-curl -L 'https://sites.google.com/site/germeval2014ner/data/NER-de-test.tsv?attredirects=0&d=1' \
-| grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > test.txt.tmp
+# GERMEVAL 2014 (german NER)
+
+# curl -L 'https://sites.google.com/site/germeval2014ner/data/NER-de-train.tsv?attredirects=0&d=1' \
+# | grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > train.txt.tmp
+# curl -L 'https://sites.google.com/site/germeval2014ner/data/NER-de-dev.tsv?attredirects=0&d=1' \
+# | grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > dev.txt.tmp
+# curl -L 'https://sites.google.com/site/germeval2014ner/data/NER-de-test.tsv?attredirects=0&d=1' \
+# | grep -v "^#" | cut -f 2,3 | tr '\t' ' ' > test.txt.tmp
+# export MAX_LENGTH=128
+# export BERT_MODEL=bert-base-multilingual-cased
+# python3 preprocess.py train.txt.tmp $BERT_MODEL $MAX_LENGTH > train.txt
+# python3 preprocess.py dev.txt.tmp $BERT_MODEL $MAX_LENGTH > dev.txt
+# python3 preprocess.py test.txt.tmp $BERT_MODEL $MAX_LENGTH > test.txt
+# cat train.txt dev.txt test.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > labels.txt
+
+# WNUT’17 (English NER)
+
+mkdir -p data_wnut_17
+
+curl -L 'https://github.com/leondz/emerging_entities_17/raw/master/wnut17train.conll'  | tr '\t' ' ' > data_wnut_17/train.txt.tmp
+curl -L 'https://github.com/leondz/emerging_entities_17/raw/master/emerging.dev.conll' | tr '\t' ' ' > data_wnut_17/dev.txt.tmp
+curl -L 'https://raw.githubusercontent.com/leondz/emerging_entities_17/master/emerging.test.annotated' | tr '\t' ' ' > data_wnut_17/test.txt.tmp
 export MAX_LENGTH=128
-export BERT_MODEL=bert-base-multilingual-cased
-python3 preprocess.py train.txt.tmp $BERT_MODEL $MAX_LENGTH > train.txt
-python3 preprocess.py dev.txt.tmp $BERT_MODEL $MAX_LENGTH > dev.txt
-python3 preprocess.py test.txt.tmp $BERT_MODEL $MAX_LENGTH > test.txt
-cat train.txt dev.txt test.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > labels.txt
+export BERT_MODEL=bert-large-cased
+
+python3 preprocess.py data_wnut_17/train.txt.tmp $BERT_MODEL $MAX_LENGTH > data_wnut_17/train.txt
+python3 preprocess.py data_wnut_17/dev.txt.tmp $BERT_MODEL $MAX_LENGTH > data_wnut_17/dev.txt
+python3 preprocess.py data_wnut_17/test.txt.tmp $BERT_MODEL $MAX_LENGTH > data_wnut_17/test.txt
+
+cat data_wnut_17/train.txt data_wnut_17/dev.txt data_wnut_17/test.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > data_wnut_17/labels.txt
