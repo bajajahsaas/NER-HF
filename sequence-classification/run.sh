d8a6f3437813d126abd8f70553909942a1339d8b
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-06-12 02:22:33 -0400

    update to bert cls

--- sequence-classification/run.sh
+++ sequence-classification/run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-
-export DATASET=w2
-export RANDOM_SEQ_LEN=3
-
+#
 #SBATCH --job-name=bert-cls
-#SBATCH --output=logscls/${DATASET}_${RANDOM_SEQ_LEN}.txt  # output file
-#SBATCH -e logscls/${DATASET}_${RANDOM_SEQ_LEN}.err        # File to which STDERR will be written
+#SBATCH --output=logscls/bert_class_%j.txt  # output file
+#SBATCH -e logscls/bert_class_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
 #SBATCH --partition=2080ti-long # Partition to submit to
 #SBATCH --mem=40GB
 #
 #SBATCH --ntasks=1
 
+export DATASET=w2
+export RANDOM_SEQ_LEN=3
+export BERT_MODEL='/mnt/nfs/work1/696ds-s20/abajaj/nlplab/long-term-context/models/bert-large-cased'
+
 export TRAIN=./data/${DATASET}-classifier-data${RANDOM_SEQ_LEN}-train.csv
 export TEST=./data/${DATASET}-classifier-data${RANDOM_SEQ_LEN}-test.csv
 
 export OUT_DIR=./${DATASET}/${RANDOM_SEQ_LEN}
 
-python -u bert_finetuning.py --trainFile=${TRAIN} --devFile=${TEST} --testFile=${TEST} --outputDir=${OUT_DIR} --gpu
+python -u bert_finetuning.py --trainFile=${TRAIN} --devFile=${TEST} --testFile=${TEST} --outputDir=${OUT_DIR} --model_name_or_path ${BERT_MODEL} --gpu
