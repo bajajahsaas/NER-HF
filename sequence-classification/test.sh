a8acfe67ee9742605dabce79cc2212267a76c30a
Ahsaas Bajaj <ahsaas.bajaj@instabase.com>
2020-06-17 02:27:52 -0400

    Updates in test script

--- sequence-classification/test.sh
+++ sequence-classification/test.sh
@@ -19,10 +19,10 @@ export NUM_LABELS=4 # 4, 3
 export MODEL=./${DATASET}/${MODE}/${RANDOM_SEQ_LEN}/model.pt
 export BERT=bert-large-cased
 
-#export TEST=./data/${DATASET}-classifier-data${RANDOM_SEQ_LEN}-test.csv
-#export OUT_DIR=./${DATASET}/${RANDOM_SEQ_LEN}/inference  # always make new directory, since code empties OUT_DIR and then writes
+#export TEST=./data/${MODE}/${DATASET}-classifier-data-test${RANDOM_SEQ_LEN}.csv
+#export OUT_DIR=./${DATASET}/${MODE}/${RANDOM_SEQ_LEN}/inference  # always make new directory, since code empties OUT_DIR and then writes
 
 export TEST=./instabase-data/w2-classifier-instabase-test.csv
-export OUT_DIR=./${DATASET}/${RANDOM_SEQ_LEN}/instabase-inference  # always make new directory, since code empties OUT_DIR and then writes
+export OUT_DIR=./${DATASET}/${RANDOM_SEQ_LEN}/instabase-data  # always make new directory, since code empties OUT_DIR and then writes
 
 python -u test_classifier.py --modelFile=${MODEL} --testFile=${TEST} --outputDir=${OUT_DIR} --model_name_or_path=${BERT} --num_labels=${NUM_LABELS} --gpu
